820e43a84e4713135bdbdd3d9e442cf72416be68
Kim-Arcena <kimanecra143@gmail.com>
2023-04-15 19:13:04 +0800

    add dotted lines

--- trackngo/lib/bottomSheet/first_bottom_sheet.dart
+++ trackngo/lib/bottomSheet/first_bottom_sheet.dart
@@ -33,86 +33,89 @@ class MyBottomSheet extends StatelessWidget {
               ),
               SizedBox(height: 20),
               Container(
-                child: Row(
-                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                  children: [
-                    Container(
-                      width: 50.0,
-                      height: 50.0,
-                      decoration: BoxDecoration(
-                        color: Color(0XFFDFF1E9),
-                        borderRadius: BorderRadius.circular(10.0),
-                      ),
-                      child: TextButton(
-                        onPressed: () {},
-                        child: Text(
-                          "1",
-                          style: TextStyle(
-                            color: Colors.black,
-                            fontSize: 20,
-                            fontWeight: FontWeight.bold,
+                child: CustomPaint(
+                  painter: DottedLinePainter(),
+                  child: Row(
+                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                    children: [
+                      Container(
+                        width: 50.0,
+                        height: 50.0,
+                        decoration: BoxDecoration(
+                          color: Color(0XFFDFF1E9),
+                          borderRadius: BorderRadius.circular(10.0),
+                        ),
+                        child: TextButton(
+                          onPressed: () {},
+                          child: Text(
+                            "1",
+                            style: TextStyle(
+                              color: Colors.black,
+                              fontSize: 20,
+                              fontWeight: FontWeight.bold,
+                            ),
                           ),
                         ),
                       ),
-                    ),
-                    Container(
-                      width: 50.0,
-                      height: 50.0,
-                      decoration: BoxDecoration(
-                        color: Color(0XFF021C0F),
-                        borderRadius: BorderRadius.circular(10.0),
-                      ),
-                      child: TextButton(
-                        onPressed: () {},
-                        child: Text(
-                          "2",
-                          style: TextStyle(
-                            color: Colors.white,
-                            fontSize: 20,
-                            fontWeight: FontWeight.bold,
+                      Container(
+                        width: 50.0,
+                        height: 50.0,
+                        decoration: BoxDecoration(
+                          color: Color(0XFF021C0F),
+                          borderRadius: BorderRadius.circular(10.0),
+                        ),
+                        child: TextButton(
+                          onPressed: () {},
+                          child: Text(
+                            "2",
+                            style: TextStyle(
+                              color: Colors.white,
+                              fontSize: 20,
+                              fontWeight: FontWeight.bold,
+                            ),
                           ),
                         ),
                       ),
-                    ),
-                    Container(
-                      width: 50.0,
-                      height: 50.0,
-                      decoration: BoxDecoration(
-                        color: Color(0XFF021C0F),
-                        borderRadius: BorderRadius.circular(10.0),
-                      ),
-                      child: TextButton(
-                        onPressed: () {},
-                        child: Text(
-                          "3",
-                          style: TextStyle(
-                            color: Colors.white,
-                            fontSize: 20,
-                            fontWeight: FontWeight.bold,
+                      Container(
+                        width: 50.0,
+                        height: 50.0,
+                        decoration: BoxDecoration(
+                          color: Color(0XFF021C0F),
+                          borderRadius: BorderRadius.circular(10.0),
+                        ),
+                        child: TextButton(
+                          onPressed: () {},
+                          child: Text(
+                            "3",
+                            style: TextStyle(
+                              color: Colors.white,
+                              fontSize: 20,
+                              fontWeight: FontWeight.bold,
+                            ),
                           ),
                         ),
                       ),
-                    ),
-                    Container(
-                      width: 50.0,
-                      height: 50.0,
-                      decoration: BoxDecoration(
-                        color: Color(0XFF021C0F),
-                        borderRadius: BorderRadius.circular(10.0),
-                      ),
-                      child: TextButton(
-                        onPressed: () {},
-                        child: Text(
-                          "4",
-                          style: TextStyle(
-                            color: Colors.white,
-                            fontSize: 20,
-                            fontWeight: FontWeight.bold,
+                      Container(
+                        width: 50.0,
+                        height: 50.0,
+                        decoration: BoxDecoration(
+                          color: Color(0XFF021C0F),
+                          borderRadius: BorderRadius.circular(10.0),
+                        ),
+                        child: TextButton(
+                          onPressed: () {},
+                          child: Text(
+                            "4",
+                            style: TextStyle(
+                              color: Colors.white,
+                              fontSize: 20,
+                              fontWeight: FontWeight.bold,
+                            ),
                           ),
                         ),
                       ),
-                    ),
-                  ],
+                    ],
+                  ),
                 ),
               ),
               child,
@@ -121,5 +124,38 @@ class MyBottomSheet extends StatelessWidget {
         );
       },
     );
+  }
+}
+
+class DottedLinePainter extends CustomPainter {
+  @override
+  void paint(Canvas canvas, Size size) {
+    Paint paint = Paint()
+      ..color = Color(0xFF021C0F)
+      ..strokeWidth = 2.7
+      ..strokeCap = StrokeCap.round
+      ..style = PaintingStyle.stroke
+      ..isAntiAlias = true
+      ..strokeJoin = StrokeJoin.round;
+
+    double dashWidth = 4;
+    double dashSpace = 5;
+    double startY = size.height / 2;
+    double endY = size.height / 2;
+    double currentX = 0;
+
+    while (currentX < size.width) {
+      canvas.drawLine(
+        Offset(currentX, startY),
+        Offset(currentX + dashWidth, endY),
+        paint,
+      );
+      currentX += dashWidth + dashSpace;
+    }
+  }
+
+  @override
+  bool shouldRepaint(CustomPainter oldDelegate) {
+    return false;
   }
 }
