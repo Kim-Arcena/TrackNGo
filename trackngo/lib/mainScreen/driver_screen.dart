a5be10c9186d5602eec59bc644d9396baae3e142
Kim-Arcena <kimanecra143@gmail.com>
2023-04-24 22:08:22 +0800

    modify driver's side

--- trackngo/lib/mainScreen/driver_screen.dart
+++ trackngo/lib/mainScreen/driver_screen.dart
@@ -1,8 +1,13 @@
 import 'dart:async';
 
 import 'package:flutter/material.dart';
+import 'package:geolocator/geolocator.dart';
 import 'package:location/location.dart';
 import 'package:google_maps_flutter/google_maps_flutter.dart';
+import 'package:provider/provider.dart';
+import 'package:trackngo/assistants/assistant_methods.dart';
+import 'package:trackngo/bottomSheet/first_bottom_sheet.dart';
+import 'package:trackngo/infoHandler/app_info.dart';
 import 'package:trackngo/tabPages/earning_tab.dart';
 import 'package:trackngo/tabPages/home_tab.dart';
 import 'package:trackngo/tabPages/ratings_tab.dart';
@@ -16,9 +21,22 @@ class MainScreen extends StatefulWidget {
 
 class _MainScreenState extends State<MainScreen>
     with SingleTickerProviderStateMixin {
+  LatLng _initialcameraposition = LatLng(20.5937, 78.9629);
+  final Completer<GoogleMapController> _controllerGoogleMap =
+      Completer<GoogleMapController>();
+  GoogleMapController? newGoogleMapController;
+  Location _location = Location();
+  LocationPermission? _locationPermission;
+  String humanReadableAddress = "";
+  bool _bottomSheetVisible = true;
+  List<LatLng> pLineCoordinatesList = [];
+  Set<Polyline> polyLineSet = {};
+  var sourceLatLng;
+  Set<Marker> markerSet = {};
+  Set<Circle> circleSet = {};
+
   TabController? tabController;
   int selectedIndex = 0;
-
   onItemClicked(int index) {
     setState(() {
       selectedIndex = index;
@@ -26,27 +44,75 @@ class _MainScreenState extends State<MainScreen>
     });
   }
 
-  @override
-  void initState() {
-    super.initState();
-    tabController = TabController(length: 3, vsync: this);
+  void _onMapCreated(GoogleMapController _cntlr) async {
+    newGoogleMapController = _cntlr;
+
+    // Get user's current location
+    Position position = await Geolocator.getCurrentPosition();
+
+    // Get human readable address for the location
+    humanReadableAddress =
+        await AssistantMethods.searchAddressForGeographicalCoordinates(
+            position, context);
+
+    // Set the camera position to the user's location
+    setState(() {
+      _initialcameraposition = LatLng(position.latitude, position.longitude);
+    });
+
+    newGoogleMapController?.animateCamera(
+      CameraUpdate.newCameraPosition(
+        CameraPosition(target: _initialcameraposition, zoom: 25),
+      ),
+    );
+
+    var sourcePosition =
+        Provider.of<AppInfo>(context, listen: false).userPickUpLocation!;
+
+    //originLatLng
+    sourceLatLng = LatLng(
+        sourcePosition.locationLatitude!, sourcePosition.locationLongitude!);
+
+    print("this is the sourceLatLng int the _onMapCreated:: $sourceLatLng");
+
+    // Load the custom PNG image
+    BitmapDescriptor customIconOrigin = await BitmapDescriptor.fromAssetImage(
+        ImageConfiguration(size: Size(48, 48)), 'images/commuter.png');
+
+    Marker originMarker = Marker(
+      markerId: const MarkerId("originID"),
+      infoWindow:
+          InfoWindow(title: sourcePosition.locationName, snippet: "Origin"),
+      position: sourceLatLng,
+      icon: customIconOrigin,
+    );
+
+    Circle originCircle = Circle(
+      circleId: const CircleId("originID"),
+      fillColor: Color(0x225add6c),
+      center: sourceLatLng,
+      radius: 25,
+      strokeWidth: 4,
+      strokeColor: Color(0x225add6c),
+    );
+
+    setState(() {
+      markerSet.add(originMarker);
+      circleSet.add(originCircle);
+    });
   }
 
-  LatLng _initialcameraposition = LatLng(20.5937, 78.9629);
-  final Completer<GoogleMapController> _controllerGoogleMap =
-      Completer<GoogleMapController>();
-  GoogleMapController? newGoogleMapController;
-  Location _location = Location();
+  checkIfLocationPermissionGranted() async {
+    _locationPermission = await Geolocator.requestPermission();
+    if (_locationPermission == LocationPermission.denied) {
+      _locationPermission = await Geolocator.requestPermission();
+    }
+  }
 
-  void _onMapCreated(GoogleMapController _cntlr) {
-    newGoogleMapController = _cntlr;
-    _location.onLocationChanged.listen((l) {
-      newGoogleMapController?.animateCamera(
-        CameraUpdate.newCameraPosition(
-          CameraPosition(target: LatLng(l.latitude!, l.longitude!), zoom: 25),
-        ),
-      );
-    });
+  void initState() {
+    super.initState();
+    checkIfLocationPermissionGranted();
+    tabController = TabController(length: 3, vsync: this);
   }
 
   @override
@@ -65,6 +131,8 @@ class _MainScreenState extends State<MainScreen>
                   mapType: MapType.normal,
                   onMapCreated: _onMapCreated,
                   myLocationEnabled: true,
+                  markers: markerSet,
+                  circles: circleSet,
                 ),
                 Positioned(
                     left: 40.0,
@@ -114,67 +182,90 @@ class _MainScreenState extends State<MainScreen>
                         ),
                       ),
                     )),
+                Positioned(
+                    right: 40.0,
+                    top: 80.0,
+                    child: Container(
+                      width: 50.0,
+                      height: 50.0,
+                      decoration: BoxDecoration(
+                        color: Colors.white,
+                        borderRadius: BorderRadius.circular(10.0),
+                        boxShadow: [
+                          BoxShadow(
+                            color: Color(0xffd4dbdd),
+                            blurRadius: 12,
+                            spreadRadius: 2,
+                            offset: Offset(0, 3),
+                          ),
+                        ],
+                      ),
+                      child: IconButton(
+                        onPressed: () {},
+                        icon: Icon(
+                          Icons.location_on_sharp,
+                        ),
+                      ),
+                    )),
+                Align(
+                    alignment: Alignment.bottomCenter,
+                    child: Container(
+                      decoration: BoxDecoration(
+                        borderRadius: BorderRadius.only(
+                          topLeft: Radius.circular(250),
+                          topRight: Radius.circular(250),
+                        ),
+                        boxShadow: [
+                          BoxShadow(
+                            color: Colors.grey.withOpacity(0.2),
+                            blurRadius: 25.0,
+                            offset:
+                                Offset(0, -15), // changes position of shadow
+                          ),
+                        ],
+                      ),
+                      child: SizedBox(
+                        height: 90,
+                        child: Material(
+                          elevation: 10,
+                          borderRadius: BorderRadius.only(
+                            topLeft: Radius.circular(30),
+                            topRight: Radius.circular(30),
+                          ),
+                          borderOnForeground: true,
+                          child: BottomNavigationBar(
+                            items: const [
+                              BottomNavigationBarItem(
+                                icon: Icon(Icons.explore),
+                                label: 'Home',
+                              ),
+                              BottomNavigationBarItem(
+                                icon: Icon(Icons.attach_money),
+                                label: 'Earnings',
+                              ),
+                              BottomNavigationBarItem(
+                                icon: Icon(Icons.settings),
+                                label: 'Settings',
+                              ),
+                            ],
+                            unselectedItemColor: Color(0xFF7c7c7c),
+                            selectedItemColor: Color(0xFF4E8C6F),
+                            backgroundColor: Color.fromARGB(255, 240, 255, 244),
+                            type: BottomNavigationBarType.fixed,
+                            selectedLabelStyle:
+                                const TextStyle(fontWeight: FontWeight.bold),
+                            showUnselectedLabels: true,
+                            currentIndex: selectedIndex,
+                            onTap: onItemClicked,
+                            elevation: 22,
+                          ),
+                        ),
+                      ),
+                    )),
               ],
             ),
-          ),
-          TabBarView(
-            physics: const NeverScrollableScrollPhysics(),
-            controller: tabController, // pass tabController to TabBarView
-            children: const [
-              HomeTabPage(),
-              EarningsTabPage(),
-              RatingsTabPage(),
-            ],
           ),
         ],
-      ),
-      bottomNavigationBar: Container(
-        decoration: BoxDecoration(
-          boxShadow: [
-            BoxShadow(
-              color: Colors.grey.withOpacity(0.2),
-              blurRadius: 25.0,
-              offset: Offset(0, -15), // changes position of shadow
-            ),
-          ],
-        ),
-        child: SizedBox(
-          height: 90,
-          child: Material(
-            elevation: 10,
-            borderRadius: const BorderRadius.only(
-              topLeft: Radius.circular(30),
-              topRight: Radius.circular(30),
-            ),
-            borderOnForeground: true,
-            clipBehavior: Clip.antiAlias,
-            child: BottomNavigationBar(
-              items: const [
-                BottomNavigationBarItem(
-                  icon: Icon(Icons.explore),
-                  label: 'Home',
-                ),
-                BottomNavigationBarItem(
-                  icon: Icon(Icons.attach_money),
-                  label: 'Earnings',
-                ),
-                BottomNavigationBarItem(
-                  icon: Icon(Icons.settings),
-                  label: 'Settings',
-                ),
-              ],
-              unselectedItemColor: Color(0xFF7c7c7c),
-              selectedItemColor: Color(0xFF4E8C6F),
-              backgroundColor: Color.fromARGB(255, 240, 255, 244),
-              type: BottomNavigationBarType.fixed,
-              selectedLabelStyle: const TextStyle(fontWeight: FontWeight.bold),
-              showUnselectedLabels: true,
-              currentIndex: selectedIndex,
-              onTap: onItemClicked,
-              elevation: 22,
-            ),
-          ),
-        ),
       ),
     );
   }
